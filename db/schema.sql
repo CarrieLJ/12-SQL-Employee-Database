2398d4c009817c975ae9424ed08441bad725bbd6
Mona Lisa <cjensen@netechnical.com>
2023-01-25 09:21:06 -0600

    added back in the primary and foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE department (
   id INT AUTO_INCREMENT NOT NULL,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
-);
+)
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL,
@@ -15,8 +15,8 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INT,
     PRIMARY KEY (id)
-    -- FOREIGN KEY (department_id) REFERENCES department(id)
-);
+    FOREIGN KEY (department_id) REFERENCES department(id)
+)
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT NOT NULL,
-    -- FOREIGN KEY (role_id) REFERENCES role(id)
-    -- PRIMARY KEY (id)
-    -- FOREIGN KEY (manager_id)
-);
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    PRIMARY KEY (id)
+    FOREIGN KEY (manager_id)
+)
